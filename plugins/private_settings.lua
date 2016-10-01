a127a2809f0e6fa2073090822a10fba34012f103
Matt <matthew@matthewhesketh.com>
2016-10-01 21:44:31 +0100

    Fixes
    Fixed some stuff that made the entire bot unusable xD

--- plugins/private_settings.lua
+++ plugins/private_settings.lua
@@ -1,5 +1,5 @@
 local function change_private_setting(user_id, key)
-    local hash = 'user:'..user_id..':settings'
+    local hash = 'user:' .. user_id .. ':settings'
     local val = 'off'
     local current_status = (db:hget(hash, key)) or config.private_settings[key]
     if current_status == 'off' then
@@ -7,9 +7,8 @@ local function change_private_setting(user_id, key)
     end
     db:hset(hash, key, val)
 end
-
 local function doKeyboard_privsett(user_id)
-    local hash = 'user:'..user_id..':settings'
+    local hash = 'user:' .. user_id .. ':settings'
     local user_settings = db:hgetall(hash)
     if not next(user_settings) then
         user_settings = config.private_settings
@@ -20,26 +19,27 @@ local function doKeyboard_privsett(user_id)
             end
         end
     end
-    
     local keyboard = {inline_keyboard = {}}
     local button_names = {
-        ['rules_on_join'] = _('Rules on join'),
-        ['reports'] = _('Users reports')
+        ['rules_on_join'] = 'Rules on join',
+        ['reports'] = 'User Reports'
     }
     for key, status in pairs(user_settings) do
         local icon
-        if status == 'on' then icon = '✅' else icon = '❌' end
-        table.insert(keyboard.inline_keyboard, {{text = button_names[key], callback_data = 'myset:alert'}, {text = icon, callback_data = 'myset:'..key}})
+        if status == 'on' then
+        	icon = '✅'
+        else	
+        	icon = '❌'
+        end
+        table.insert(keyboard.inline_keyboard, {{text = button_names[key], callback_data = 'myset:alert'}, {text = icon, callback_data = 'myset:' .. key}})
     end
-    
     return keyboard
 end
-
 local function action(msg, blocks)
     if not msg.cb then
         if msg.chat.type == 'private' then
             local keyboard = doKeyboard_privsett(msg.from.id)
-            api.sendKeyboard(msg.from.id, _('Change your private settings'), keyboard, true)
+            api.sendKeyboard(msg.from.id, 'Change your private settings.', keyboard, true)
         end
     end
     if msg.cb then
@@ -49,11 +49,10 @@ local function action(msg, blocks)
             change_private_setting(msg.from.id, blocks[1])
             local keyboard = doKeyboard_privsett(msg.from.id)
             api.editMarkup(msg.from.id, msg.message_id, keyboard)
-            api.answerCallbackQuery(msg.cb_id, _('⚙ Setting changed'))
+            api.answerCallbackQuery(msg.cb_id, '⚙ Setting changed.')
         end
     end
 end
-
 return {
     action = action,
     triggers = {
